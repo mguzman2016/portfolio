a86b44590ad2b4458c4e5fdb50aa2f561424987c
mguzman2016 <miguelguzman.tob@gmail.com>
2025-10-14 14:01:26 +0200

    fixing file location

--- src/linkedin_etl/run_etl.sh
+++ src/linkedin_etl/run_etl.sh
@@ -20,7 +20,7 @@ source "$VENV/bin/activate"
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 $PYTHON -m pip install -U pip >/dev/null 2>&1 || true
-$PYTHON etl/main.py >> "$LOGDIR/etl_$timestamp.log" 2>&1
+$PYTHON main.py >> "$LOGDIR/etl_$timestamp.log" 2>&1
 
 cd "$LOGDIR"
 ls -1t etl_*.log | tail -n +21 | xargs -r rm --
